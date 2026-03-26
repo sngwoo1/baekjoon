c73b7a5a8fe97366b80c09df65761e605eb3e871
sngwoo1 <ssaby311@gmail.com>
2026-03-26 19:20:14 +0900

    [Silver IV] Title: 카드2, Time: 8 ms, Memory: 3068 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#define MAX_SIZE 500001
+typedef int Element;
+
+Element data[MAX_SIZE];
+int front, rear;
+
+void init_queue()
+{
+    front = 0;
+    rear = 0;
+}
+
+int is_empty(void)
+{
+    return (front == rear);
+}
+
+int is_full(void)
+{
+    return (front == (rear+1)%MAX_SIZE);
+}
+
+void enqueue(Element e)
+{
+    if (is_full())
+        printf("overflow\n");
+    rear = (rear+1) % MAX_SIZE;
+    data[rear] = e;
+}
+
+Element dequeue(void)
+{
+    if (is_empty())
+        printf("underflow\n");
+    front = (front+1) % MAX_SIZE;
+
+    return data[front];
+}
+
+Element peek(void)
+{
+    if (is_empty())
+        printf("underflow\n");
+    Element value = (front+1) % MAX_SIZE;
+
+    return data[value];
+}
+
+
+
+int main(void)
+{
+    init_queue();
+    int N;
+
+    scanf("%d", &N);
+
+    for(int i = 1; i <= N; i++)
+        enqueue(i);
+    
+    int result = 1;
+    while(!is_empty())
+    {
+        result = dequeue();
+
+        if (!is_empty())
+        {
+            enqueue(dequeue());
+        }
+    }
+    printf("%d\n", result);
+
+    return 0;
+
+
+}
