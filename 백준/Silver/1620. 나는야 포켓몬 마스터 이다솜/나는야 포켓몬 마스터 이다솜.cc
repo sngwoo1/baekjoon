64235f5b7c23ae1a77adf8e013f311b6948a7a3d
sngwoo1 <ssaby311@gmail.com>
2026-04-05 21:38:34 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 108 ms, Memory: 20860 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -1,32 +1,29 @@
-#include <iostream>
-#include <map>
-#include <string>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-string a[100001], s;
-map<string, int> mp;
 int N, M;
+string s;
+map<string, int> mp;
+string a[100004];
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-
-	cin >> N >> M;
-	for (int i = 1; i <= N; i++) {
-		cin >> a[i];
-		mp[a[i]] = i;
-	}
-
-	for (int i = 0; i < M; i++) {
-		cin >> s;
-		if (s[0] >= '0' && s[0] <= '9') {
-			cout << a[stoi(s)] << '\n';
-		}
-
-		else {
-			cout << mp[s] << '\n';
-		}
-	}
-
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
+    cin >> N >> M;
+    
+    for(int i = 0; i < N; i++) {
+        cin >> s;
+        mp[s] = i + 1;
+        a[i+1] = s;
+    }
+    
+    for(int i = 0; i < M; i++) {
+        cin >> s;
+        if(isdigit(s[0])) cout << a[stoi(s)] << '\n';
+        else {
+            cout << mp[s] << '\n';
+        }
+    }
+    
+    return 0;
 }
